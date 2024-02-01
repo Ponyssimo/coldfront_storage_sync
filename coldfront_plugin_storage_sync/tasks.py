dab5446602c0e8541f4e03276988b5421f4ca3ca
Jeffrey Condell <jdcrc@rit.edu>
2024-02-01 15:30:20 -0500

    added attribute retrieval and a basic structure

--- coldfront_plugin_storage_sync/tasks.py
+++ coldfront_plugin_storage_sync/tasks.py
@@ -1,4 +1,16 @@
 from coldfront.core.allocation.models import Allocation
 
-def add_storage_allocation():
-    pass
+def add_storage_allocation(allocation_pk):
+    allocation = Allocation.objects.get(pk=allocation_pk)
+    project = allocation.project.name
+    size = allocation.get_attribute("Storage Guota (GB)")
+
+    # convert GB to bytes
+    size = size * 1073741824
+
+    #Figure out how to create CLAWS group and modify the autofs file here
+
+    #run createDirectory and save exit code to status
+    status = NULL
+
+    #report error if status !=0
