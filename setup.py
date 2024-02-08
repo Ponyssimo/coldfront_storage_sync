3780c41ae4bffddf94752a7604b58a3476eb1a32
Jeffrey Condell <jdcrc@rit.edu>
2024-02-08 14:29:50 -0500

    Added basic setup file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setuptools
+
+setup(
+    name='coldfront_storage_sync',
+    url='https://github.com/Ponyssimo/coldfront_storage_sync',
+    author='Jeffrey Condell',
+    packages=['coldfront_storage_sync']
+)
