19258b692889c78dcad53ccd46e3d143723e2bed
Jeffrey Condell <jdcrc@rit.edu>
2024-03-07 16:18:43 -0500

    removed signal testing from signals

--- coldfront_plugin_storage_sync/signals.py
+++ coldfront_plugin_storage_sync/signals.py
@@ -5,14 +5,9 @@ from coldfront.core.allocation.signals import allocation_activate, allocation_ch
 
 from coldfront.core.allocation.views import AllocationCreateView
 
-import logging
-
-logger = logging.getLogger(__name__)
 
 @receiver(allocation_activate)
 @receiver(allocation_change_approved)
 def activate_allocation(sender, **kwargs):
-    logger.warn("test1")
-    open("/tmp/test.txt", "x")
-    #allocation_pk = kwargs.get('allocation_pk')
-    #async_task('coldfront_plugin_storage_sync.tasks.add_storage_allocation',allocation_pk)
+    allocation_pk = kwargs.get('allocation_pk')
+    async_task('coldfront_plugin_storage_sync.tasks.add_storage_allocation',allocation_pk)
