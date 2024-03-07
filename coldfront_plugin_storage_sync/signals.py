3ca7899dfe48a7f8a3affce5bcd39c41bc3f1371
Jeffrey Condell <jdcrc@rit.edu>
2024-03-07 10:09:44 -0500

    added test file creation to signals

--- coldfront_plugin_storage_sync/signals.py
+++ coldfront_plugin_storage_sync/signals.py
@@ -13,5 +13,6 @@ logger = logging.getLogger(__name__)
 @receiver(allocation_change_approved)
 def activate_allocation(sender, **kwargs):
     logger.warn("test1")
+    open("/tmp/test.txt", "x")
     #allocation_pk = kwargs.get('allocation_pk')
     #async_task('coldfront_plugin_storage_sync.tasks.add_storage_allocation',allocation_pk)
