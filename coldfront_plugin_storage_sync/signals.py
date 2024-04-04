d53121327478fe8ab2a8b3ad0bd66de2b1514b7e
Jeffrey Condell <jdcrc@rit.edu>
2024-04-03 22:25:09 -0400

    added checks for if allocation is storage and setting default storage quota for new allocations

--- coldfront_plugin_storage_sync/signals.py
+++ coldfront_plugin_storage_sync/signals.py
@@ -1,13 +1,27 @@
 from django.dispatch import receiver
 from django_q.tasks import async_task
 
-from coldfront.core.allocation.signals import allocation_activate, allocation_change_approved
+from coldfront.core.allocation.signals import allocation_activate, allocation_change_approved, allocation_new
 
 from coldfront.core.allocation.views import AllocationCreateView
 
+def is_storage(allocation_pk):
+    resource = Allocation.objects.get(allocation_pk).get_parent_resource
+    if resource.name == 'CEPH':
+        return True
+    return False
+
+#add default storage quota when allocation created, but before approved - allocation_new
+@receiver(allocation_new)
+def new_storage(sender, **kwargs):
+    allocation_pk = kwargs.get('allocation_pk')
+    if is_storage(allocation_pk):
+        allocation = Allocation.objects.get(pk=allocation_pk)
+        allocation.set_usage("Storage Quota (GB)", 150)
 
 @receiver(allocation_activate)
 @receiver(allocation_change_approved)
 def activate_allocation(sender, **kwargs):
     allocation_pk = kwargs.get('allocation_pk')
-    async_task('coldfront_plugin_storage_sync.tasks.add_storage_allocation',allocation_pk)
+    if is_storage(allocation_pk):
+        async_task('coldfront_plugin_storage_sync.tasks.add_storage_allocation',allocation_pk)
