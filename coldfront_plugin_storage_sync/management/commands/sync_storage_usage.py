83f1c87afd38784b275597b85ee7322f522a6f07
Jeffrey Condell <jdcrc@rit.edu>
2024-05-13 14:37:35 -0400

    removed noop

--- coldfront_plugin_storage_sync/management/commands/sync_storage_usage.py
+++ coldfront_plugin_storage_sync/management/commands/sync_storage_usage.py
@@ -21,11 +21,8 @@ logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     help = "sync storage usage"
-    def add_arguments(self, parser):
-        parser.add_argument("-n", "--noop", help="Print commands only. Do not run any commands.", action="store_true")
     
     def handle(self, *args, **options):
-        self.noop = False
         if options['noop']:
             self.noop = True
             logger.warn("No operations will be committed")
